a1310a6941aab44219096ceaeac8dfb758657f50
llll3 <90038489+llll3@users.noreply.github.com>
2022-03-23 21:15:06 +0300

    Add files via upload

---
+++ inf-settings.py
@@ -0,0 +1,21 @@
+import time
+import os
+gg = True
+while gg:
+    print('Выберите пункт:')
+    print('1.Установить inf.')
+    print('2.Обновить inf.')
+    q = input('--> ')
+    if q == '1':
+        gg = False
+        print('Установка inf...')
+        time.sleep(2)
+        download = os.system('cd ~ ; cd ../usr/etc ; pkg install git ; git clone https://github.com/llll3/bash ; cd bash ; cp bash.bashrc .. ; source bash.bashrc ; cd ~ ; yes | pkg update ; yes | pkg upgrade ; yes | pkg install nano ; yes | pkg install python ; yes | pkg install python2 ; yes | pkg install python3 ; cd ~ ; git clone https://github.com/llll3/inf ; cd inf ; rm -r .git -f ; chmod 777 inf ; ./inf')
+        print('Установка завершенна!')
+    elif q == '2':
+        gg = False
+        print('Обновление inf...')
+        time.sleep(2)
+        update = os.system('cd ~ ; cd ../usr/etc/ ; rm -r bash.bashrc -f ; rm -r bash -f ; git clone https://github.com/llll3/bash ; cd bash ; cp bash.bashrc .. ; source ../usr/etc/bash.bashrc ; cd ~ ; yes | pkg update ; yes | pkg upgrade ; yes | pkg install nano ; yes | pkg install python ; yes | pkg install python2 ; yes | pkg install python3 ; cd ~ ; rm -r inf -f ; git clone https://github.com/llll3/inf ; cd inf ; rm -r .git -f ; chmod 777 inf ; ./inf')
+        print('Обновление завершенно!')
+
