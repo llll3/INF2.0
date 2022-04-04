461fa592061e4c01f32321e8f67629b7c2c89bc5
llll3 <akubanov19@gmail.com>
2022-04-05 02:22:44 +0300

    qwerty

--- q
+++
@@ -1,9 +0,0 @@
-f = open("numbers.txt", "r")
-h = int(input('lohin'))
-h -= 1
-x = 0
-for line in f:
-    if x == h: #строка
-        print(line.split()[0]) #слово в строке
-        print(h)
-f.close()
--- test.py
+++ test.py
@@ -1,148 +1,146 @@
 import time, sys, os, subprocess, re
-list = str(list(range(1, 30)))
-def glav():
-    print('''
-1.spisok
-2.add
-3.delete
-4.zamenit''')
-    asa = input('aaaa: ')
-    if asa == '1':
-        listt()
-        glav()
-    elif asa == '2':
-        add1()
-    elif asa == '3':
-        delete1()
-    elif asa == '4':
-        Zamen()
+list1 = str(list(range(2, 31)))
+list2 = str(list(range(1, 5)))
+ff = "('clear')"
+def Z():
+ os.system(ff)
+ print('''
+[&] ?
+[0] Выход
+[1] список
+[2] добавить
+[3] удалить
+[4] редактировать''')
+ asa = input('-->: ')
+ if asa == '0':
+  sys.exit()
+ if asa == '1':
+  z1()
+ elif asa == '2':
+  z2()
+ elif asa == '3':
+  z3()
+ elif asa == '4':
+  z4()
+ elif asa != list2:
+  Z()
 
-def listt():
- nanu()
- with open('nanu.txt', 'r') as f3:
+def z1():
+ os.system(ff)
+ z1_2()
+ print('''
+[&] Список
+[0] Назад''')
+ with open('nanu.txt') as f3:
   for n, line in enumerate(f3, 1):
+   kk = '[&] Список:\n[0] Назад'
    line1 = line.rstrip('\n')
    print(f'[{n}] {line1}')
-def nanu():
-   with open('name.txt', 'r') as f1, open('numbers.txt', 'r') as f2, open('nanu.txt', 'w') as f3:
-       for i, j in zip(f1.readlines(), f2.readlines()):
-           f3.write(f'{i.strip()}: {j}')
-def delete1():
- listt()
- stroka1()
- stroka2()
- hhh = str(hh)
- if hhh in list:
-  with open('name.txt') as f1:
-      lines1 = f1.readlines() # список текста в линию
-  strr1 = str(nomer1)
-  str1 = strr1
-  pattern1 = re.compile(re.escape(str1)) # re.complite()
-  with open('name.txt', 'r+') as ff1: # открытие файла
-    for line in lines1: # для line это lines1
-        result1 = pattern1.search(line) # None
-        if result1 is None: # None?
-           ff1.write(line) # [Name]
-        delete2()
-def delete2():
-  with open('numbers.txt') as f2:
-      lines2 = f2.readlines()
-  strr2 = str(nomer2)
-  str2 = strr2
-  pattern2 = re.compile(re.escape(str2))
-  with  open('numbers.txt', 'r+') as ff2:
-    for line in lines2:
-        result2 = pattern2.search(line)
-        if result2 is None:
-            ff2.write(line)
-        print(f'Удaленно: {nomer1}')
-        print(f'Удаленно: {nomer2}')
-        glav()
+ xx = input('-->')
+ if xx == '0':
+  Z()
+ if xx >= '1' and not  xx > '0' :
+  z1()
+ else:
+  z1()
 
-def stroka1():
- with open("name.txt", "r") as ff1:
-  jj = input('Строка: ')
-  h = int(jj)
-  h -= 1
-  global hh
-  hh = h
-  x = 0
-  for line1 in ff1:
-   if x == h: #строка
-    global nomer1
-    nomer1 = line1.split()[0] #слово в строке
-    print(nomer1)
-   x += 1
+def z1_2():
+ File1 = 'names.txt'
+ File2 = 'numbers.txt'
+ File3 = 'nanu.txt'
+ TemporaryFile3 = File3 + '.tmpp' # создаём файл
+ os.system("touch %s" % TemporaryFile3) # временный файл
+ with open(File1, 'r') as f1, open(File2, 'r') as f2, open(TemporaryFile3, 'w') as f4:
+  for i, j in zip(f1.readlines(), f2.readlines()):
+   f4.write(f'{i.strip()}: {j}')
+ path3 = os.path.join(os.path.abspath(os.path.dirname(__file__)), File3)
+ os.remove(path3) # удаляем основной файл
+ os.system("mv %s %s" % (TemporaryFile3, File3))
 
-def stroka2():
- with open("numbers.txt", "r") as ff2:
-  x = 0
-  for line2 in ff2:
-   if x == hh: #строка
-    global nomer2
-    nomer2 = line2.split()[0] #слово в строке
-    print(nomer2)
-   x += 1
+def z3():
+ os.system(ff)
+ File1 = 'names.txt'
+ File2 = 'numbers.txt'
+ z3z4_2(File1, File2)
+ hh = int(jek)
+ hh -= 1
+ if str(hh) in list1:
+  with open(File1) as f1, open(File2) as ff1:
+   lines1 = f1.readlines() # список текста в линию
+   lines2 = ff1.readlines() # [(1, 2, 3, 4, 5)]
+   del lines1[hh]
+   del lines2[hh]
+  with open(File1, 'w') as f1, open(File2, 'w') as ff1: # открытие файла
+   f1.writelines(lines1)
+   ff1.writelines(lines2)
 
-def add1():
-        with open('numbers.txt','a') as f,  open('name.txt', 'a') as f1:
-            xx = input('Введите имя: ')
-            f1.write(f'\n{xx}')
-            xxx = input('Введите номер: ')
-            f.write(f'\n{xxx}')
-            print(f'Добавлено имя: {xx}')
-            print(f'Добавлен номер: {xxx}')
+def z2():
+ os.system(ff)
+ File1 = 'names.txt'
+ File2 = 'numbers.txt'
+ with open(File1,'a') as f1,  open(File2, 'a') as ff1:
+  xx = input('Введите имя: ')
+  f1.write(f'\n{xx}')
+  xxx = input('Введите номер: ')
+  ff1.write(f'\n{xxx}')
+  print(f'Добавлено имя: {xx}')
+  print(f'Добавлен номер: {xxx}')
 
-def Delete(File1, File2):
-  listt()
-  jek = input('Строка: ')
-  if jek in list:
-    with open(File1, 'r') as fff1, open(File2, 'r') as fff2:
-      h = int(jek) ; h -= 1 ; h = h ; x = 0 ; xx = 0
-      for line1 in fff1:
-          if x == h: #строка
-            global nomer1
-            nomer1 = line1.split()[0]
-          x += 1
-      for line2 in fff2:
-          if xx == h: #строка
-            global nomer2
-            nomer2 = line2.split()[0] #слово в строке
-          xx += 1
+def z3z4_2(File1, File2):
+ z1()
+ global jek
+ jek = input('[&] Удаленние:\n[0] Назад\nВведите п.номер: ')
+ if jek in list1:
+  with open(File1) as fff1, open(File2) as fff2:
+   h = int(jek)
+   h -= 1
+   h = h
+   x = 0
+   xx = 0
+   for line1 in fff1:
+    if x == h: #строка
+     global nomer1
+     nomer1 = line1.split()[0]
+    x += 1
+   for line2 in fff2:
+    if xx == h: #строка
+     global nomer2
+     nomer2 = line2.split()[0] #слово в строке
+    xx += 1
 
-def Delete2(File1, File2, FindThis1, FindThis2, new1, new2):
-        TemporaryFile1 = File1 + '.tmp' ; TemporaryFile2 = File2 + '.tmp'   # создаём файл
-        os.system("touch %s" % TemporaryFile1) ; os.system("touch %s" % TemporaryFile2)    # временный файл
-        with open(File1, 'r') as f1, open(TemporaryFile1, 'w') as f2, open(File2, 'r') as ff1, open(TemporaryFile2, 'w') as ff2:
-            lines1 = f1.readlines() ; lines2 = ff1.readlines()
-            for line1 in lines1:
-                line1 = line1.strip()
-                if line1 == FindThis1:
-                    f2.write(f'{new1}') # меняем строку
-                else:
-                    f2.write(line1 + '\n') # оставляем прежнюю
+def z4_3(File1, File2, FindThis1, FindThis2, new1, new2):
+ os.system(ff)
+ TemporaryFile1 = File1 + '.tmp' ; TemporaryFile2 = File2 + '.tmp'   # создаём файл
+ os.system("touch %s" % TemporaryFile1) ; os.system("touch %s" % TemporaryFile2)    # временный файл
 
-            for line2 in lines2:
-                line2 = line2.strip()
-                if line2 == FindThis2:
-                    ff2.write(f'{new2}') # меняем строку
-                else:
-                    ff2.write(line2 + '\n') # оставляем прежнюю
+ with open(File1, 'r') as f1, open(TemporaryFile1, 'w') as f2, open(File2, 'r') as ff1, open(TemporaryFile2, 'w') as ff2:
+  lines1 = f1.readlines() ; lines2 = ff1.readlines()
+  for line1 in lines1:
+   line1 = line1.strip()
+   if line1 == FindThis1:
+    f2.write(f'{new1}') # меняем строку
+   else:
+    f2.write(line1 + '\n') # оставляем прежнюю
+  for line2 in lines2:
+   line2 = line2.strip()
+   if line2 == FindThis2:
+    ff2.write(f'{new2}') # меняем строку
+   else:
+    ff2.write(line2 + '\n') # оставляем прежнюю
 
-        path1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), File1) ; path2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), File2)
-        os.remove(path1) ; os.remove(path2) # удаляем основной файл
-        os.system("mv %s %s" % (TemporaryFile1, File1)) ; os.system("mv %s %s" % (TemporaryFile2, File2)) # переименовываем временный файл
+ path1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), File1) ; path2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), File2)
+ os.remove(path1) ; os.remove(path2) # удаляем основной файл
+ os.system("mv %s %s" % (TemporaryFile1, File1)) ; os.system("mv %s %s" % (TemporaryFile2, File2)) # переименовываем временный файл
 
-def Zamen():
-  File1 = 'name.txt'
-  File2 = 'numbers.txt'
-  Delete(File1, File2)
-  FindThis1 = nomer1
-  FindThis2 = nomer2
-  new1 = input('Введите новое имя: ')
-  new2 = input('Введите новый номер: ')
-  result = Delete2(File1, File2, FindThis1, FindThis2, new1, new2)
+def z4():
+ os.system(ff)
+ File1 = 'names.txt'
+ File2 = 'numbers.txt'
+ z3z4_2(File1, File2)
+ FindThis1 = nomer1
+ FindThis2 = nomer2
+ new1 = input('[&] Редактировать:\n[0] Назад\nВведите новое имя: ')
+ new2 = input('Введите новый номер: ')
+ result12 = z4_3(File1, File2, FindThis1, FindThis2, new1, new2)
 
-while list:
-    glav()
-glav()
+Z()
